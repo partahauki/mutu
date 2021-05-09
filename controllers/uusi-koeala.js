d680016f8ee607de54d9b7792101dc3ec3590cd5
partahauki <75426609+partahauki@users.noreply.github.com>
2021-05-09 18:23:00 +0300

    suutu jo
    git jumittaa

--- controllers/uusi-koeala.js
+++ controllers/uusi-koeala.js
@@ -10,18 +10,15 @@ exports.controller = (event, command, args) => {
 }
 
 createAla = (event, args) => {
-    let insert = db2.prepare(`INSERT INTO koealat VALUES(null,
-        '${args["koealaName"]}','${args["avain"]}' , false, false)`)
+    let insert = db2.prepare(`INSERT INTO koealat VALUES(null,'${args["koealaName"]}','${args["avain"]}' , false, false)`)
     let koealaId = insert.run().lastInsertRowid
 
-    let select = db2.prepare(`SELECT * FROM link_koelalat_kaominaisuudet 
-        WHERE id_koealat = ${args["templateId"]}`)
+    let select = db2.prepare(`SELECT * FROM link_koelalat_kaominaisuudet WHERE id_koealat = ${args["templateId"]}`)
     let templateLinks = select.all()
 
     insert = "INSERT INTO link_koelalat_kaominaisuudet VALUES "
     templateLinks.forEach(row => {
-        insert += (`(${koealaId},${row["id_kaominaisuudet"]},
-            ${row["tiedot"]}),`)
+        insert += (`(${koealaId},${row["id_kaominaisuudet"]},${row["tiedot"]}),`)
     })
     insert = insert.slice(0, -1)
     insert = db2.prepare(insert).run()
